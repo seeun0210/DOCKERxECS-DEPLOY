532af7d34eea5105d36f3ca7bb4a40e854f1b503
kimseeun <cocobell3@naver.com>
2024-01-09 23:57:17 +0900

    feat: express로 간단한 웹서버 만들기

--- EXPRESS/app/index.ts
+++ EXPRESS/app/index.ts
@@ -1,1 +1,15 @@
-console.log("hello express");
+import express from "express"
+
+const app=express();
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.status(200).send("hello from express")
+})
+
+const PORT=4000;
+
+app.listen(PORT,()=>{
+    console.log(`App listening at port ${PORT}`)
+})
--- EXPRESS/build/index.js
+++ EXPRESS/build/index.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("hello express");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.get("/", (req, res) => {
+    res.status(200).send("hello from express");
+});
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`App listening at port ${PORT}`);
+});
